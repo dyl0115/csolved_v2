79415133f20cc56fedacd837adbb3712fc7c5796
doubleDRG <dyl660115@gmail.com>
2025-11-10 13:25:52 +0900

    [refact] 댓글, 대댓글 토글 버튼

--- src/main/resources/static/js/post-detail.js
+++ src/main/resources/static/js/post-detail.js
@@ -1,40 +1,3 @@
-// 좋아요 버튼
-// async function addLike(postType, postId)
-// {
-//     const likeButton = document.getElementById("like-button");
-//     const likeCountElement = document.getElementById("like-count");
-//     let likeCount = parseInt(likeCountElement.textContent, 10) || 0;
-//
-//     const response = await fetch(`/api/${postType}/${postId}/likes`,
-//         {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//         });
-//
-//     switch (response.status)
-//     {
-//         case 200:
-//             likeCount++;
-//             likeCountElement.textContent = likeCount;
-//             likeButton.disabled = true;
-//             break;
-//         case 409:
-//             const modal = new bootstrap.Modal(document.getElementById("addLikeConflictModal"));
-//             modal.show();
-//             const closeModalButton = document.getElementById("closeModalButton");
-//             closeModalButton.addEventListener("click", () =>
-//             {
-//                 modal.hide();
-//             });
-//             break;
-//         default:
-//             alert('좋아요 처리 중 오류가 발생했습니다.');
-//             break;
-//     }
-// }
-
 // 북마크 토글 버튼
 async function toggleBookmark(bookmarkButton)
 {
@@ -67,31 +30,6 @@ async function toggleBookmark(bookmarkButton)
     }
 }
 
-// 게시글 삭제
-// async function deletePost(postType, pluralPostType, postId)
-// {
-//     try
-//     {
-//         const response = await fetch(`/api/${postType}/${postId}`, {
-//             method: 'DELETE',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             }
-//         });
-//
-//         if (!response.ok)
-//         {
-//             throw new Error('삭제에 실패했습니다.');
-//         }
-//         window.location.replace(`/${pluralPostType}?page=1`);
-//
-//     }
-//     catch (error)
-//     {
-//         console.error('Error:', error);
-//         alert('삭제 중 오류가 발생했습니다.');
-//     }
-// }
 
 // 답글 삭제
 async function deleteAnswer(answerId)
@@ -145,21 +83,21 @@ async function deleteComment(commentId)
 }
 
 // 댓글 토글 기능
-document.getElementById('toggle-answer-form').addEventListener('click', function ()
-{
-    const card = document.getElementById('answer-form-card');
-    card.classList.toggle('hidden');
-});
+// document.getElementById('toggle-answer-form').addEventListener('click', function ()
+// {
+//     const card = document.getElementById('answer-form-card');
+//     card.classList.toggle('hidden');
+// });
 
 // 대댓글 토글 기능
-document.querySelectorAll('.toggle-comment-form').forEach(element =>
-{
-    element.addEventListener('click', function ()
-    {
-        const commentForm = this.nextElementSibling;
-        commentForm.classList.toggle('hidden');
-    });
-});
+// document.querySelectorAll('.toggle-comment-form').forEach(element =>
+// {
+//     element.addEventListener('click', function ()
+//     {
+//         const commentForm = this.nextElementSibling;
+//         commentForm.classList.toggle('hidden');
+//     });
+// });
 
 // 이미지 크기 조절
 document.addEventListener('DOMContentLoaded', function ()
