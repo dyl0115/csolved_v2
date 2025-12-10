b7cf8f1f894374c8491a6d92a9c5d2d2e4cffeb3
doubleDRG <dyl660115@gmail.com>
2025-12-10 16:01:48 +0900

    [save] .

--- src/main/resources/static/js/editor/quill-editor.js
+++ src/main/resources/static/js/editor/quill-editor.js
@@ -129,7 +129,7 @@ function initQuillEditor(selector)
         }
     }
 
-    quill = new Quill(selector, config);
+    const quill = new Quill(selector, config);
     quill.root.style.minHeight = '300px';
     quill.root.setAttribute("spellcheck", 'false');
     quill.format('size', '15px');
